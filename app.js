566b8b68297f2deafbce23607ece9de9845ff886
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-12 02:35:24 +0400

    Places Router created

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-toutes");
+const usersRoutes = require("./routes/users-routes");
 
 const app = express();
 
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes);
 
 app.listen(5000);
