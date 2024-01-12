9b8b865d4501264fdfe0d0a70cd36990890afc2f
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-12 15:49:02 +0400

    Update Route Create

---
+++ routes/places-routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const placesControllers = require("../controllers/places-controller");
+
+router.get("/:pid", placesControllers.getPlaceById);
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
+router.post("/", placesControllers.createPlace);
+
+router.patch("/:pid", placesControllers.updatePlace);
+router.put("/:pid", placesControllers.deletePlace);
+
+module.exports = router;
