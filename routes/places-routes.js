8cafafb1c0902470e2b375c847e3dcac329e4000
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-13 20:43:46 +0400

    Validating API Input

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 const placesControllers = require("../controllers/places-controller");
 
 router.get("/:pid", placesControllers.getPlaceById);
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
-router.post("/", placesControllers.createPlace);
+router.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").length({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  placesControllers.createPlace
+);
 
 router.patch("/:pid", placesControllers.updatePlace);
 router.delete("/:pid", placesControllers.deletePlace);
