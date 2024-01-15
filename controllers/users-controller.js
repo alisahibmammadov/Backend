c475f3d39cf6f48a2b763b6178ff600dc1c6f960
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-15 18:57:16 +0400

    Hashed Password

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,6 +1,8 @@
 const { validationResult } = require("express-validator");
 const { HttpError } = require("../models/http-error");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const getUsers = async (req, res, next) => {
   let users;
@@ -39,11 +41,18 @@ const signup = async (req, res, next) => {
     );
     return next(error);
   }
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Could not create user, please try again", 500);
+    return next(error);
+  }
   const createdUser = new User({
     name,
     email,
     image: "URL",
-    password,
+    password: hashedPassword,
     place: [],
   });
 
@@ -53,8 +62,14 @@ const signup = async (req, res, next) => {
     const error = new HttpError("Creating user failed, please try again", 500);
     return next(error);
   }
+
+
+  
+
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
+
+
 const login = async (req, res, next) => {
   const { email, password } = req.body;
   let existingUser;
@@ -68,10 +83,28 @@ const login = async (req, res, next) => {
     );
     return next(error);
   }
-  if (!existingUser || existingUser.password !== password) {
+
+  if (!existingUser) {
     const error = new HttpError(
       "User exists already, please login instead.",
       422
+    );
+    return next(error);
+  }
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials and try again",
+      500
+    );
+    return next(error);
+  }
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials and try again",
+      500
     );
     return next(error);
   }
